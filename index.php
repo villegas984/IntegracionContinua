e3697e647284b03ebb5f5d200949084297a2f26a
villegas984 <villegas984@hotmail.com>
2022-12-07 22:41:00 -0500

    attempt8

--- index.php
+++ index.php
@@ -156,7 +156,7 @@ function consultaTalla(){
 <!--fin barra navegacion-->
 <!--seccion de productos-->
 <div class="panel-body">
-	<h1>Jenkins Eres Muy Grande y poderoso</h1>
+	<h1>Jenkins Eres Muy poderoso</h1>
     <h2>Mis Productos</h2>
     <a href="VerCarta.php" class="cart-link" title="Ver Carta"><i class="glyphicon glyphicon-shopping-cart"></i></a>
     <div id="products" class="row list-group-horizontal">
